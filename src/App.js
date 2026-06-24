b943001efaca94f6af5491db9642a43033e54a10
Salome Carval <salomecarval6@gmail.com>
2026-06-24 19:26:08 +0530

    Successfully added all NewsApp project files and resolved submodule tracking conflict

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react'
+import NavBar from './NavBar';
+//here NavBar is not inside components folder else 
+// import NavBar from './components/NavBar';
+import News from './News';
+import NewsItem from './News';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
+
+/*
+V.imp this all is taken from News setProgress={setProgress} API doc where as per cateory the url changes with apiKey const
+
+Pass this as props 
+
+//Tech-Headlines
+https://News setProgress={setProgress}data.io/api/1/latest? 
+  apikey=pub_82cca6454fb54bf5bb4fc11d72a0947c
+  &country=us
+  &category=technology
+
+
+//Positive tesla coverage
+  https://News setProgress={setProgress}data.io/api/1/latest? 
+  apikey=pub_82cca6454fb54bf5bb4fc11d72a0947c
+  &q=tesla
+  &category=business
+  &sentiment=positive
+
+//Climate chnage updates
+  https://News setProgress={setProgress}data.io/api/1/archive? 
+  apikey=pub_82cca6454fb54bf5bb4fc11d72a0947c
+  &q=climate change
+  &from_date=2025-09-30
+
+
+  //Global market url format:
+  https://News setProgress={setProgress}data.io/api/1/market? 
+  apikey=pub_82cca6454fb54bf5bb4fc11d72a0947c
+  &q=market
+  &language=en
+  &sort=pubdateasc
+  &size=5
+*/
+
+const App=()=> {
+
+
+const myAPIkey=process.env.REACT_APP_API_KEY;
+
+const[progress,setProgress]=useState(0)
+
+
+  
+    return (
+      <Router>-
+      <div>
+        <NavBar/>
+        <LoadingBar
+        height={3}
+        color="#f11946"
+        progress={progress}
+        //onLoaderFinished={() => setProgress(0)}
+      />
+
+        {/* You MUST wrap your Route components inside Routes */}
+        <Routes>
+          <Route path="/Breaking" element={<News setProgress={setProgress} key="breaking" country="in" category="breaking"  myAPIkey={myAPIkey}/>} />
+          <Route path="/Business" element={<News setProgress={setProgress} key="business" country="in" category="business"  myAPIkey={myAPIkey}/>} />
+          <Route path="/Crime" element={<News setProgress={setProgress} key="crime" country="in" category="crime"  myAPIkey={myAPIkey}/>} />
+          <Route path="/Domestic" element={<News setProgress={setProgress} key="domestic" country="in" category="domestic"  myAPIkey={myAPIkey}/>} />
+          <Route path="/Education" element={<News setProgress={setProgress} key="education" country="in" category="education"  myAPIkey={myAPIkey} />} />
+          <Route path="/Entertainment" element={<News setProgress={setProgress} key="entertainment" country="in" category="entertainment"  myAPIkey={myAPIkey}/>} />
+          <Route path="/Health" element={<News setProgress={setProgress} key="health" country="in" category="health"  myAPIkey={myAPIkey}/>} />
+        </Routes>
+       
+      </div>
+    </Router>
+    )
+  
 }
 
-export default App;
+
+export default App
---
+++ src/NewsItem.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+
+/*
+The NewsItem component is a dumb, presentation card. It does not manage its own shifting data. 
+It simply sits there and waits for the News component to pass down data via props (like a title or an image link).
+Because it has no local state to initialize, delete the constructor here. It just needs a standard render() method:
+
+*/
+
+
+
+    // It directly reads what it was given by its parent
+    let {title,description,imgUrl, moreInfo, author,date,source}=props;//destructing 
+//here title,description..are passed as props and not as state since we wont dynamically
+//want to change it instead want it to keep constant.Props cant be changed
+
+
+    return (
+      <div className='my-3'>
+        <div className="card" /*style={{width: '18rem'}}*/>
+
+         <div style={{display:'flex', justifyContent:'flex-end',position:'absolute',right:'0'}}>
+          <span className=" badge rounded-pill bg-danger" >{source}
+                <span className="visually-hidden">unread messages</span>
+            </span> 
+         </div>
+          
+          <img src={imgUrl} className="card-img-top" alt="..." 
+            style={{ height: '200px', objectFit: 'cover' }} />
+            {/*Style is added to img to display uniform img */}
+            
+           
+           <div className="card-body"> 
+              <h5 className="card-title text-dark">{title}...</h5>
+              <p className="card-text">{description}...</p>
+              <p class="card-text"><small class="text-body-secondary">By {author} on {new Date(date).toGMTString()}</small></p>
+              <a rel="noreferrer" href={moreInfo} target="_blank" className="btn btn-sm btn-primary">Read More..</a>
+           </div> 
+        </div>
+      </div>
+    )
+  
+}
+//The curly braces {moreInfo} inside your JSX layout tell React: "Stop reading this code as regular
+//HTML text, drop into JavaScript mode, and find the value stored inside the moreInfo variable."
+export default NewsItem
